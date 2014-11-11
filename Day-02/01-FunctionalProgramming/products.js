22540b7f8ba27374d230890e44276519d6229740
tkmagesh <tkmagesh77@gmail.com>
2014-11-11 09:50:57 +0530

    Functional Programming - Seed

---
+++ Day-02/01-FunctionalProgramming/products.js
@@ -0,0 +1,8 @@
+var products = [
+    {id : 3, name : "Pen", cost : 30, units : 20, category : 2},
+    {id : 8, name : "Hen", cost : 60, units : 60, category : 1},
+    {id : 5, name : "Den", cost : 40, units : 40, category : 2},
+    {id : 9, name : "Ten", cost : 50, units : 70, category : 1},
+    {id : 4, name : "Len", cost : 20, units : 30, category : 2},
+    {id : 6, name : "Zen", cost : 70, units : 80, category : 1}
+]
